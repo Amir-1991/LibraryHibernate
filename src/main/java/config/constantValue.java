6aa98bac193ce18b621829e4fef6dc493ea99f49
amir <amir.abedi@rocketmail.com>
2021-08-15 14:44:52 +0430

    Add Config File And Add many value

---
+++ src/main/java/config/constantValue.java
@@ -0,0 +1,11 @@
+package config;
+
+public class constantValue {
+
+    public static String nationalCodeRegex = "\\d{10}";
+    public static String phoneNumberRegex = "\\d{11}";
+    public static String menuRegex = "\\d{1}";
+    public static String dateFormatterRegex = "yyyy-MM-dd";
+    public static String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+
+}
