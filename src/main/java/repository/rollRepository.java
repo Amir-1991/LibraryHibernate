a7c50eb065056f3e9974a46388d0c0cd56ac6fae
amir <amir.abedi@rocketmail.com>
2021-08-17 22:35:31 +0430

    roll fetch complete

---
+++ src/main/java/repository/rollRepository.java
@@ -0,0 +1,24 @@
+package repository;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class rollRepository {
+    public static List loadRoll() {
+        Configuration configuration = new Configuration().configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("select roll.rollTitle from rollEntity roll where roll.rollTitle not like :rollAdmin");
+        query.setParameter("rollAdmin", "admin");
+        List<String> rollList = query.getResultList();
+        transaction.commit();
+        session.close();
+        return rollList;
+    }
+}
